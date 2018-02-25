a5a28faf57ed853a4d3862f63aa161c9bf6cdae7
Petr Kočí <petr.koci@samizdat.cz>
2018-02-25 09:25:39 +0100

    opravené barvičky, do grafů přidaná druhá zlatá Ledecké

--- R/analyse.R
+++ R/analyse.R
@@ -30,9 +30,9 @@ medailiste <- medailiste %>%
 medailiste$random <- runif(nrow(medailiste),-0.25,0.25)
 
 toJSON(medailiste %>%
-  filter(medaile=="zlato") %>%
-  mutate(x=i+random) %>%
-  select(x,y=vek,j=jmeno,d=disciplina,p=pohlavi))
+         filter(medaile=="zlato") %>%
+         mutate(x=i+random) %>%
+         select(x,y=vek,j=jmeno,d=disciplina,p=pohlavi))
 
 toJSON(medailiste %>%
          filter(medaile=="stříbro") %>%
@@ -44,4 +44,18 @@ toJSON(medailiste %>%
          mutate(x=i+random) %>%
          select(x,y=vek,j=jmeno,d=disciplina,p=pohlavi))
 
-  
+categories2 <- c("1924 Chamonix","1928 Svatý Mořic","1932 Lake Placid","1936 Garmisch-Partenkirchen","1948 Svatý Mořic","1952 Oslo","1956 Cortina d'Ampezzo","1960 Squaw Valley","1964 Innsbruck","1968 Grenoble","1972 Sapporo","1976 Innsbruck","1980 Lake Placid","1984 Sarajevo","1988 Calgary","1992 Albertville","1994 Lillehammer","1998 Nagano","2002 Salt Lake City","2006 Turín","2010 Vancouver","2014 Soči","2018 Pchjongčchang")
+
+medaile <- gs_read(data, "Kopie listu medaile")
+
+medaile$y <- as.integer(!is.na(medaile$m))
+
+medaile <- medaile %>%
+  arrange(o,m,d)
+
+medaile$i <- rep(1:8)
+
+toJSON(medaile %>%
+         left_join(data.frame(m=c("bronz","stříbro","zlato"), color=c("rgb(205,127,50)", "rgb(192,192,192)", "rgb(255, 215, 0)"))) %>%
+         filter(i==1) %>%
+         select(y,j,d,m,color))
